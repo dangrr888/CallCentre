0ccb4e6f14154aaae46bc4d29ffc2017eea67e9a
Daniel Cumberbatch <appledoughnut@gmail.com>
2017-06-26 21:52:06 +0100

    Added documentation for Call class

--- src/include/main/Call.h
+++ src/include/main/Call.h
@@ -1,45 +1,103 @@
 #ifndef GUARD_SRC_INCLUDE_MAIN_CALL_H_
 #define GUARD_SRC_INCLUDE_MAIN_CALL_H_
 
-#include <boost/serialization/strong_typedef.hpp>
+#include <string>
 
-#includec<string>
+#include <boost/serialization/strong_typedef.hpp>
 
 namespace callcentre
 {
 
-  class Call
+  /// @brief A class representing a call made to
+  ///  a call centre.
+  class final Call
   {
   public:
+
+    // public aliases
+
+    /// @brief Call severity, used within a
+    ///   an implementation specific metric
+    ///   that will be used to determine the
+    ///   nececessary experience and rank of
+    ///   a respondent that can close such a
+    ///   call.
     BOOST_STRONG_TYPEDEF(std::size_t, Severity);
+
+    /// @brief Call identification.
     BOOST_STRONG_TYPEDEF(std::size_t, Id);
 
-    /* structors */
+    // public structors
+
+    /// @brief 4-parameter constructor.
+    /// @param id Id of this Call.
+    /// @param caller_name The name of the caller.
+    /// @param description A brief description of the
+    ///    the purpose of the call.
+    /// @param severity The severity of the call.
     Call( const Id& id
         , const std::string& caller_name
         , const std::string& description
         , const Severity& severity
         );
+
+    /// @brief Copy constructor.
+    /// @note Default implementation.
     Call(const Call&) = default;
+
+    /// @brief Move constructor.
+    /// @note Default implementation.
     Call(Call&&) = default;
+
+    /// @brief Copy assignment operator.
+    /// @note Default implementation.
     Call& operator=(const Call&) = default;
+
+    /// @brief Move assignment operator.
+    /// @note Default implementation.
     Call& operator=(Call&&) = default;
 
+    /// @brief Destructor.
+    /// @note Default implementation.
     ~Call() = default;
 
-    /* publis etters */
+    // Public etters
+
+    /// @brief Return the id of this Call.
+    /// @retval The id of this Call.
     const Id& id() const;
+
+    /// @brief Return the name of the caller
+    ///   associated with this Call.
+    /// @retval The name of the caller.
     const std::string& caller_name() const;
+
+    /// @brief Return a description of this Call.
+    /// @retval The description of this Call.
     const std::string& description() const;
+
+    /// @brief Return the severity of this Call.
+    /// @retval The severity of this Call.
     const Severity& severity() const;
+
+    /// @brief Set the severity of this Call.
+    /// @param severity The severity to set.
     void severity(const Severity& severity);
 
   private:
 
-    /* data members */
+    // Private data members.
+
+    /// @brief The id of this Call.
     Id m_id;
+
+    /// @brief The caller name.
     const std::string m_caller_name;
+
+    /// @brief The description of this Call.
     const std::string m_description;
+
+    /// @brief The severity of this Call.
     Severity m_severity;
 
   }; // ! class Call
